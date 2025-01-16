d60d60be292f6395c03bda5220c3862abfdfb5be
evopos2024 <info@evopos.co>
2025-01-16 14:53:28 -0500

    Actualizacion enrutamiento

--- core/Router.php
+++ core/Router.php
@@ -6,7 +6,7 @@ class Router
     $uri = trim(parse_url($uri, PHP_URL_PATH), '/');
     $segments = explode('/', $uri);
 
-    $controllerName = !empty($segments[0]) ? ucfirst($segments[0]) . 'Controller' : 'HomeController';
+    $controllerName = !empty($segments[0]) ? ucfirst($segments[0]) . 'Controller' : 'SupportRequestController';
     $methodName = isset($segments[1]) ? $segments[1] : 'index';
     $params = array_slice($segments, 2);
 
